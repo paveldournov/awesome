ab749dfa14e7e523a9399c0b7da9e96ca827bd79
Pavel Dournov <paveld@gmail.com>
2018-09-08 17:50:27 -0700

    Adding sample grapthic program

---
+++ g1.py
@@ -0,0 +1,36 @@
+from graphics import *
+import random
+import time
+
+def main():
+    win = GraphWin("Investment Growth", 640, 480)
+    win.setBackground("blue")
+    #win = GraphWin("Circle", 100, 100)
+
+
+#    while True:
+#        p = win.getMouse()
+#        c = Circle(p, 5)
+#        c.setFill("yellow")
+#        c.draw(win)
+
+    colors = ["yellow", "purple", "green", "white", "orange", "red", "black", "pink"]
+    print(colors[0])
+    print(colors[6])
+
+
+    for i in range(1,100):
+        x = random.randint(1, 640)
+        y = random.randint(1, 480)
+        r = random.randint(1,40)
+        ci = random.randint(0, 6)
+        cs = colors[ci]
+        c = Circle(Point(x,y), r)
+        c.setFill(cs)
+        c.draw(win)
+        time.sleep(0.03)
+
+    win.getMouse()
+    win.close()
+
+main()
